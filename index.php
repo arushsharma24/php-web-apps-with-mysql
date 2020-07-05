a982d5c3310136fc1be0d06701538751f2ce1d30
arushsharma <arush24astar@gmail.com>
2020-07-05 21:24:13 +0530

    remove deleted files

--- index.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <title>Arush Sharma - Autos Database</title>
-</head>
-<h1>Welcome to Autos Database</h1>
-<a href="login.php">Please Log In</a>
-<p>Attempt to go to <a href="autos.php">autos.php</a> without logging in - it should fail with an error message</p>
--- pdo.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<pre>\n";
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 
-'foo', 'bar');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "</pre>";
